a5c45de7ec864e593e01ec7a8e1485d73be65a31
Juan De Oliveira <juanabrahamd@gmail.com>
2022-08-22 19:32:16 +0200

    WIP

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const miscController = require('../controllers/misc.controller');
 
 // Misc
-
 router.get('/', miscController.home);
+
+// Auth > Register
+router
 
 module.exports = router; // ???
---
+++ models/User.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose'); // Para crear DB
+const bcrypt = require('bcrypt'); // Para encriptar la contraseña del usuario
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    image: {
+        type: String,
+        default: './images/user-default.png'
+    }
+
+})
+
+module.exports = User;
